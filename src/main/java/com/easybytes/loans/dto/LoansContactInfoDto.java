9d6f4793e6c73ec4554464bc2cf9170ba9037ae8
Louisojibe <louisojibe@gmail.com>
2026-07-21 17:11:38 +0100

    added management endpoints and rabbitmq for busRefresh

--- src/main/java/com/easybytes/loans/dto/LoansContactInfoDto.java
+++ src/main/java/com/easybytes/loans/dto/LoansContactInfoDto.java
@@ -1,10 +1,19 @@
 package com.easybytes.loans.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
+@Setter
+@Getter
 @ConfigurationProperties(prefix = "loans")
-public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class LoansContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+
 }
